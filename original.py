3cb23fabc507c22dae65221ad05aa2deb7ed65ed
Gemnen <mr.bulgat12@mail.ru>
2022-08-02 10:32:03 +0300

    new original.py

--- original.py
+++ original.py
@@ -19,19 +19,10 @@
 # 2. Влияние прироста добавленной стоимости в разрезе видов экономической деятельности на ВТП в определенные
 #    периоды моделирования, процент (deltaVDST)
 import pandas as pd
-import pandas
 from typing import Dict, List
 import openpyxl
 from openpyxl.styles import Alignment
-from tkinter.tix import NoteBook
-from matplotlib.pyplot import title
-from matplotlib.pyplot import legend
 import plotly.express as px
-import matplotlib
-import plotly 
-import numpy as np
-from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
-import matplotlib.pyplot as plt
 import doctest 
 
 document_inner = pd.read_excel("C:/Users/pc/.vscode/gitwork/histograms/doctest_files/data.xlsx")
@@ -271,7 +262,7 @@ def get_prognos(data):
     return data
 
 
-def get_axis_x(data_df: pandas.DataFrame,start_year: int) -> List:
+def get_axis_x(data_df: pd.DataFrame,start_year: int) -> List:
     '''
     Функция формирует список со значениями(axis_x) для оси Х(горизонтальной)
     На вход функции подается Data Frame со значениями бюджета и категориями,
@@ -280,7 +271,7 @@ def get_axis_x(data_df: pandas.DataFrame,start_year: int) -> List:
 
     >>> dict = {'ВТП Казани, тыс. руб.': [674739900.0, 748722900.0, 773031100.0, 798154610.75, 824094635.6, 850877711.26, 878531236.88], 'Добавленная стоимость, тыс. руб.': [191057445.0, 221584931.0, 226103515.0, 230716026.71, 235422633.65, 240225255.38, 245125850.59], 'ДС Обрабатывающие производства, тыс. руб.': [75373868.0, 80169194.0, 76066267.0, 78783598.344, 79958359.172, 79850189.785, 81156818.408], 'ДС Строительство, тыс. руб.': [8376613.0, 9553660.0, 8194075.0, 8888142.345, 9065600.551, 8888954.769, 9131323.671], 'ДС Транспортировка и хранение, тыс. руб.': [26272378.0, 35407329.0, 2987596.0, 22157415.832, 20690880.642, 15470346.997, 19884660.344], 'ДС Оптовая и розничная торговля, тыс. руб.': [19963824.0, 25665497.0, 36181429.0, 27718999.183, 30453953.188, 32137036.174, 30691239.965], 'ДС Деятельность в области информатизации и связи, тыс. руб.': [6646511.0, 9461344.0, 10263705.0, 8946153.853, 9756847.568, 9857412.822, 9709393.347]}
     >>> start_year = 2017
-    >>> data_df = pandas.DataFrame(dict) 
+    >>> data_df = pd.DataFrame(dict) 
     >>> get_axis_x(data_df,start_year)
     ['2017', '2018', '2019', '2020', '2021', '2022', '2023']
     '''
@@ -290,7 +281,7 @@ def get_axis_x(data_df: pandas.DataFrame,start_year: int) -> List:
     return axis_x
 
 
-def make_histogram(data_df: pandas.DataFrame, axis_x: List):
+def make_histogram(data_df: pd.DataFrame, axis_x: List):
     '''
     Функция формирует значения для построения графиков
     и строит графики, сохраняя их с заданным расширением 
